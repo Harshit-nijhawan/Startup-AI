ef8ca4e74f1a4e8c3bc31b60e47261d12ad500cf
Harshit <nijhawanharshit58@gmail.com>
2026-04-30 21:00:32 +0530

    fix: make CORS more aggressive and expose headers

--- app/main.py
+++ app/main.py
@@ -41,8 +41,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.ALLOWED_ORIGINS,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 # Include Routers
